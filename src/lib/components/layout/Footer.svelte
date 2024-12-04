a38ee290d3d43954a19402c28aa29b8b7f1fbf50
Caznix <caznix01@gmail.com>
2024-12-04 18:43:05 -0500

    fix invalid href

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -9,7 +9,7 @@ export let footerItems = [
 <footer class="footer bottom-0 text-center padding p-4 border-white/20 border-t-2 bg-black text-white w-full">
     <div class="flex items-center space-x-4">
         {#each footerItems as Item}
-        <a href="Item.href">
+        <a href="{Item.href}" class="text-white hover:text-gray-300">
             <span>{Item.label}</span>
         </a>
     {/each}
