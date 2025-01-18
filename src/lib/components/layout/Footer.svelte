30f561a5970720fdff83203f3251dd371e7851ab
Caznix <caznix01@gmail.com>
2025-01-17 19:36:44 -0500

    update bento and footer styling

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-    import type { Icon } from "lucide-svelte";
-
+    import {Heart, Icon,} from "lucide-svelte";
+    import type {Component} from 'svelte';
     interface FooterItem {
         href: string;
         label: string;
-        icon?: Icon
+        icon?: Component
     }
     let { footerItems = [
     {
         href: "/",
-        label: "placeholder"
+        label: "Placeholder"
     }
-] } = $props();
+] }: { footerItems?: FooterItem[] } = $props();
 </script>
-<footer class="footer bottom-0 text-center padding p-4 border-white/20 border-t-2 bg-black text-white w-full">
-    <div class="flex items-center space-x-4">
-        {#each footerItems as Item}
-        <a href="{Item.href}" class="text-white hover:text-gray-300">
-            <span>{Item.label}</span>
-            {#if Item.icon}
-            <Item.icon class="w-6 h-6" />
-            {/if}
-        </a>
-    {/each}
-</div>
+<footer class="footer bottom-0 text-center padding p-4 border-white/20 border-t-2 bg-black w-full">
+    <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-4">
+            {#each footerItems as Item}
+                <a href="{Item.href}" class="text-neutral-500 hover:text-neutral-700">
+                    <span>{Item.label}</span>
+                    {#if Item.icon}
+                        <Item.icon class="w-6 h-6" />
+                    {/if}
+                </a>
+            {/each}
+        </div>
+
+        <span class="group inline-flex items-center gap-1">
+            <span class="text-neutral-500 group-hover:text-neutral-700">Made with</span>
+            <Heart class="text-red-900 group-hover:text-red-950" />
+            <span class="text-neutral-500 group-hover:text-neutral-700">by Caznix and the open source community</span>
+        </span>
+    </div>
 </footer>
